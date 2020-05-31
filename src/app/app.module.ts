39d6e9341301730e9839dd3d71998eff2127317b
vandathron <scientistrabzy@gmail.com>
2020-05-31 22:35:54 +0100

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PlansComponent } from './plans/plans.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PlansComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/IPlan.ts
@@ -0,0 +1,8 @@
+
+export interface IPlan{
+    title: string;
+    isBronzeSupported: boolean;
+    isSilverSupported: boolean;
+    isGoldSupported: boolean;
+    hasColor: boolean;
+}
